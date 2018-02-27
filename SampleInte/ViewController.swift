ebd57c14cabf5f672d72e15a072ff5395667c345
shilu francis <shilufrancis@a2ml17461m.local>
2018-02-27 14:57:22 +0530

    Initial Commit

---
+++ SampleInte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleInte
+//
+//  Created by shilu francis on 27/02/18.
+//  Copyright © 2018 shilu francis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
